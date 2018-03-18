a5ba151badd3c88ed8a266c5d5622ceda9a0b3ee
Patryk Sadowski <patsadow2@gmail.com>
2018-03-18 07:29:13 +0100

    Chat

--- Droid/Assets/Content/src/pages/home.vue
+++ Droid/Assets/Content/src/pages/home.vue
@@ -1,45 +1,49 @@
 <template>
   <div id="home">  
 
- <ul class="messages" v-chat-scroll>
-    <li
-      v-bind:class="{ 'input-message': todo.isInput, 'output-message': todo.isOutput }"
-      v-for="(todo, index) in todos"
-      v-bind:key="todo.id"
-      v-bind:title="todo.title"
-      v-on:remove="todos.splice(index, 1)"
-    >{{todo.title}}</li>
-  </ul>
+  <ul class="messages" v-chat-scroll>
+      <li
+        v-bind:class="{ 'input-message': todo.isInput, 'output-message': todo.isOutput }"
+        v-for="(todo, index) in todos"
+        v-bind:key="todo.id"
+        v-bind:title="todo.title"
+        v-on:remove="todos.splice(index, 1)">{{todo.title}}
+        </li>
+    </ul>
 
-  <input
-    v-model="newTodoText"
-    v-on:keyup.enter="addNewTodo"
-    placeholder="Send message"
-  >
+<div class="enter-send-message">
+    <input
+      class="enter-message"
+      v-model="newTodoText"
+      v-on:keyup.enter="addNewTodo"
+      placeholder="Send message">
+    <button
+       class="send-message"
+      v-on:click="addNewTodo">Send message</button>
+  </div>
 
 
-<div class="main">
-  <h1>Kinga :)</h1>
-  <p>Some text some tesdsdxt some text.</p>
-</div>
-<div class="navbar">
-  <a href="#home" class="active"><img class="icons" src="https://image.flaticon.com/icons/svg/733/733614.svg"></a>
-  <a href="#news"><img class="icons" src="https://image.flaticon.com/icons/svg/733/733614.svg"></a>
-  <a href="#contact"><img class="icons" src="https://image.flaticon.com/icons/svg/733/733614.svg"></a>
-</div>
-</div>
+  <div class="navbar">
+    <a href="#home" class="active"><div class="icons" /></a>
+    <a href="#news"><div class="icons" /></a>
+    <a href="#contact"><div class="icons" /></a>
+  </div>
+
+  </div>
 </template>
 
 <script>
  export default {
   methods: {
     addNewTodo: function () {
-      this.todos.push({
-        id: this.nextTodoId++,
-        title: this.newTodoText,
-        isOutput: true,
-        isInput: false
-      })
+      if(this.newTodoText!=''){
+        this.todos.push({
+          id: this.nextTodoId++,
+          title: this.newTodoText,
+          isOutput: true,
+          isInput: false
+        })
+      }
       this.newTodoText = ''
     }
   },
@@ -74,59 +78,5 @@
 </script>
 
 <style scope>
-body {
-    display: block;
-    margin: 8px;
-}
-
-.icons{
-  width: 40px;
-  height: 40px;
-}
-.messages{
-  
-    background-color: lightblue;
-    width: 110px;
-    height: 110px;
-    overflow: scroll;
-}
-.input-message{
-  color: red;
-}
-
-.output-message{
-  color:blue;
-}
-.navbar {
-  overflow: hidden;
-  background-color: #333;
-  position: fixed;
-  bottom: 0;
-  width: 100%;
-  left: 0;
-}
-
-.navbar a {
-  float: left;
-  display: block;
-  color: #f2f2f2;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-  height: 40px;
-}
-
-.navbar a:hover {
-  background: #f1f1f1;
-  color: black;
-}
-
-.navbar a.active {
-  background-color: #4CAF50;
-  color: white;
-}
-
-
-
+@import './../css/home.css';
 </style>
